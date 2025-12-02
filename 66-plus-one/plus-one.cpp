f1618ca649bb2613a3f3d235d270d0fab8aff10b
Harshit Masiwal <147537521+harshitmasiwal@users.noreply.github.com>
2025-12-02 22:23:36 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (14.71%) - LeetSync

--- 66-plus-one/plus-one.cpp
+++ 66-plus-one/plus-one.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-       int n = digits.size();
+        int s = digits.size()-1;
 
-        for(int i = n - 1; i >= 0; i--) {
-            if(digits[i] < 9) {
-                digits[i]++;
-                return digits;  // no carry, done
-            }
-            digits[i] = 0;  // set to 0 and continue carry
+        for(int i = s ; i >= 0 ; i--){
+            if(digits[i] < 9){
+            digits[i] += 1;
+            return digits;
+           }
+            digits[i] = 0 ;
+
         }
+       
+        
+        digits.insert(digits.begin(),1);
 
-        // If all digits were 9 → we need extra 1 at front
-        digits.insert(digits.begin(), 1);
         return digits;
     }
 };
