c6dff1a2a02b73df3c34bad7a8b491582d1c0889
Harshit Masiwal <147537521+harshitmasiwal@users.noreply.github.com>
2026-01-03 23:43:43 +0530

    Time: 373 ms (12.67%) | Memory: 18.2 MB (20.67%) - LeetSync

---
+++ 1527-number-of-ways-to-paint-n-3-grid/number-of-ways-to-paint-n-3-grid.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+
+    int M = 1e9 +7 ;
+    vector<vector<int>>t;
+
+    string states[12] = {"RYG", "RGY", "RYR", "RGR",
+         "YRG", "YGR", "YGY", "YRY", "GRY", "GYR", "GRG", "GYG"};
+
+
+    int solve(int n , int prev){
+
+        //base case of finding 1 way
+        if(n == 0 ) return 1;
+
+        if(t[n][prev] != -1)
+            return t[n][prev];
+
+        int result = 0 ;
+        string last = states[prev];
+
+        for(int curr = 0 ; curr < 12 ; curr++){
+            if(curr == prev){
+                continue; //skip
+            }
+
+            string currPattern = states[curr]; 
+            bool conflict = false;
+            for(int col = 0 ; col < 3 ; col++){
+
+                if(currPattern[col] == last[col]){
+                    conflict = true;
+                    break;
+                }
+            }
+
+            if(!conflict){
+                result = (result + solve(n-1,curr)) % M ; 
+            }
+
+        }
+        return t[n][prev] = result;
+
+    }
+
+
+    int numOfWays(int n) {
+        
+        t.resize(n, vector<int>(12, -1));
+        int result = 0 ;
+
+        for(int i = 0 ; i < 12 ;i++){
+            //JUST CHOOSING THE FIRST ROW 
+            result = (result + solve(n-1,i)) % M ; 
+        }
+
+        return result;
+    
+    }
+};
