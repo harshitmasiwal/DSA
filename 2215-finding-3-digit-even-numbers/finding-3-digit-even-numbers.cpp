da0498403f6123a82364d58e7e8a36f1c6a5fbde
Harshit Masiwal <147537521+harshitmasiwal@users.noreply.github.com>
2026-04-14 00:11:30 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (81.07%) - LeetSync

--- 2215-finding-3-digit-even-numbers/finding-3-digit-even-numbers.cpp
+++ 2215-finding-3-digit-even-numbers/finding-3-digit-even-numbers.cpp
@@ -1,45 +1,46 @@
 class Solution {
 public:
+    vector<int>ans;
+    void solve(int pos , vector<int>&digits, long long curr , vector<int>&map){
+
+        if(pos == 3 ){
+            ans.push_back(curr);
+            return;
+        }
+
+
+        for(int i = 0 ; i < 10 ; i++){
+
+            if(map[i] == 0) continue;
+
+            if( i == 0 && pos == 0) continue;
+
+            if( pos == 2 ){
+                //check if i is even or not 
+                if(i % 2 != 0 ) continue;
+            }
+
+            //nahi to find the all possible combination
+
+
+                map[i]--;
+
+                solve(pos+1,digits,curr * 10 + i , map);
+
+                map[i]++;
+            
+
+        }
+
+        return ;
+
+    }
     vector<int> findEvenNumbers(vector<int>& digits) {
-
-        int map[10] = {0};
-
+        vector<int>map(10,0);
         for(int i = 0 ; i < digits.size() ;i++){
             map[digits[i]]++;
         }
-
-        //now
-        vector<int>ans ;
-
-        for(int i = 1 ; i <= 9 ; i++){
-            if(map[i] == 0){
-                continue;
-            }
-            map[i]--;
-            for(int j = 0 ; j <= 9 ; j++){
-                if(map[j] == 0){
-                    continue;
-                }
-                map[j]--;
-                for(int k = 0 ; k < 10 ; k+=2){
-
-                    if(map[k] == 0){
-                        continue;
-                    }
-                    map[k]--;
-
-                    int num = (i * 100 )+ (j * 10) + k ;
-
-                    ans.push_back(num);
-
-                    map[k]++;
-
-                }
-                map[j]++;
-            }
-            map[i]++;
-        }
-        
+        solve(0 , digits , 0 , map);
         return ans;
     }
 };
