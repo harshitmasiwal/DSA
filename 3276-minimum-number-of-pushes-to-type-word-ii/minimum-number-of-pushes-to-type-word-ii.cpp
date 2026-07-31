d1330d22117ed16c049e6ef6fcfefb6b49eeefb2
Harshit Masiwal <147537521+harshitmasiwal@users.noreply.github.com>
2026-07-31 12:15:52 +0530

    Time: 2 ms (94.58%) | Memory: 25.2 MB (94.44%) - LeetSync

---
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+        
+        vector<int>arr(26,0);
+
+        for(int i = 0 ; i < word.size() ; i++){
+
+            arr[word[i]-97]++;
+
+        }
+
+        sort(arr.begin(),arr.end(),greater<int>());
+        int ans = 0 ;
+        for(int i = 0 ; i < 8 ; i++){
+            ans += arr[i];
+        }
+        for(int i = 8 ; i < 16 ; i++){
+            ans += arr[i]*2;
+        }
+        for(int i = 16 ; i < 24 ; i++){
+            ans += arr[i]*3;
+        }
+        for(int i = 24 ; i < 26 ; i++){
+            ans += arr[i]*4;
+        }
+
+        return ans;
+    }
+};
